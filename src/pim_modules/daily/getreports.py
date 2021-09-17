a59a35adc362576cb337b25a0c206776e57e7e86
Mirco Schoenfeld <mirco.schoenfeld@tum.de>
2021-09-17 05:45:02 +0200

    v. 0.1.2

---
+++ src/pim_modules/daily/getreports.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+
+import os.path
+import configparser
+from . import CONF_FILE
+
+def parse_entries(conf):
+  entries = []
+  if conf.has_section('daily_statusreport'):
+    entries.extend(conf['daily_statusreport']['files'].split())
+  return entries
+
+def getreports():
+  config = configparser.ConfigParser()
+  config.read(CONF_FILE)
+  attchmnts = [_f for _f in parse_entries(config) if os.path.isfile(_f)]
+  return attchmnts if len(attchmnts) > 0 else False
+  
+if __name__ == '__main__':
+  print(getreports())
