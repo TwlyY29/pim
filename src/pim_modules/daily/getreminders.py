6c868e3e49f60876cb1d416511d9cae42e62da4b
Mirco Schoenfeld <mirco.schoenfeld@tum.de>
2021-09-20 09:28:02 +0200

    v. 0.1.2

--- src/pim_modules/daily/getreminders.py
+++ src/pim_modules/daily/getreminders.py
@@ -1,4 +1,7 @@
 import os
+import sys
+import configparser
+import importlib
 
 from datetime import date
 from datetime import datetime
@@ -7,9 +10,8 @@ from string import Template
 from subprocess import CalledProcessError, check_output
 
 from pim_mailhelper.functions import parseSender, replyStatus, getMailTextAndCharset
-import configparser
 
-from . import OUTFILE, CONF_FILE
+from . import OUTFILE, CONF_FILE, SUBMODS
 
 TEXT='''$customtitle$reminders'''
 REMINDERLINE='* %s\n'
@@ -19,7 +21,11 @@ RMDTITLE='customtitle'
 RMDLIST='reminders'
 RMDCMD='cmd'
 
+RMDMOD='mod'
+RMDSEC='sec'
+
 __filepath = os.path.dirname(os.path.realpath(__file__))
+
 
 def parse_title(title):
   return title+'\n\n'
@@ -32,11 +38,17 @@ def parse_command(cmd):
   
 def parse_entries(conf):
   entries = []
+  referrers = tuple(SUBMODS.keys())
   for sec in conf.sections():
-    if sec.startswith('daily_') and sec != 'daily_statusreport':
-      title = parse_title(conf[sec]['title'])
-      command = parse_command(conf[sec]['command'])
-      entries.append({RMDTITLE:title,RMDCMD:command})
+    if sec.startswith('daily_') and sec != 'daily_attachments':
+      if not sec.startswith(referrers):
+        title = parse_title(conf[sec]['title'])
+        command = parse_command(conf[sec]['command'])
+        entries.append({RMDTITLE:title,RMDCMD:command})
+      else:
+        key = sec[0:sec.rfind('_')]
+        if SUBMODS[key]:
+          entries.append({RMDMOD:key, RMDSEC:sec})
   return entries
 
 def getreminders():
@@ -44,22 +56,34 @@ def getreminders():
   config = configparser.ConfigParser()
   config.read(CONF_FILE)
   CMDS = parse_entries(config)
+  appended_sys_path = False
   
   for cmd in CMDS:
-    try:
-      reminder = check_output([cmd[RMDCMD]], shell=True)
-      reminder = str(reminder, "utf-8")
-      if reminder is None or "No reminders" in reminder: 
-        continue
-      else:
-        reminder = reminder.strip().split('\n')
-        reminders = ''
-        for r in reminder:
-          reminders += REMINDERLINE % r
-        customtitle = "\n\n{}".format(cmd[RMDTITLE]) if cmd[RMDTITLE] is not '' else ''
-        reminder = Template(TEXT).substitute({RMDTITLE:customtitle,RMDLIST:reminders})
-        output = "{}{}".format(output,reminder)
-    except CalledProcessError as e:
-      pass
-  return output if output is not '' else False
+    if RMDCMD in cmd:
+      try:
+        reminder = check_output([cmd[RMDCMD]], shell=True)
+        reminder = str(reminder, "utf-8")
+        if reminder is None or "No reminders" in reminder: 
+          continue
+        else:
+          reminder = reminder.strip().split('\n')
+          reminders = ''
+          for r in reminder:
+            reminders += REMINDERLINE % r
+          customtitle = "\n\n{}".format(cmd[RMDTITLE]) if cmd[RMDTITLE] != '' else ''
+          reminder = Template(TEXT).substitute({RMDTITLE:customtitle,RMDLIST:reminders})
+          output = "{}{}".format(output,reminder)
+      except CalledProcessError as e:
+        pass
+    else:
+      if not appended_sys_path:
+        sys.path.append(__filepath)
+        appended_sys_path = True
+      themod = importlib.import_module(cmd[RMDMOD])
+      customtitle, reminders = themod.compile_output(cmd[RMDSEC], CONF_FILE)
+      customtitle = "\n\n{}\n".format(customtitle) if customtitle != '' else ''
+      reminder = Template(TEXT).substitute({RMDTITLE:customtitle,RMDLIST:reminders})
+      output = "{}{}".format(output,reminder)
+        
+  return output if output != '' else False
   
